173bb6f114cab67672f27822db116536e1094a68
unknown <walterarielpelourson@yahoo.com.ar>
2025-03-19 11:56:49 -0300

    Quinto del proyecto App_Ecommerce

--- mi_proyecto/app.py
+++ mi_proyecto/app.py
@@ -1,4 +1,4 @@
-from flask import Flask #redirect, url_for
+from flask import Flask # #redirect, url_for
 from flask import render_template #redirect, url_for
 from flask import request #redirect, url_for
 import csv
